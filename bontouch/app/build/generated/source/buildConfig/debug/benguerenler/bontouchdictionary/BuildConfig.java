b57bad7c73ffac5b169ca2a3d6023863711dd54a
Bengu Erenler <benguerenler@sabanciuniv.edu>
2018-10-31 16:09:05 +0100

    upload

---
+++ bontouch/app/build/generated/source/buildConfig/debug/benguerenler/bontouchdictionary/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package benguerenler.bontouchdictionary;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "benguerenler.bontouchdictionary";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
